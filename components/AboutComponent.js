654c21fc8387475e59a6d6baf002b171c1645218
Morticia007 <jacquelyn07@mac.com>
2021-03-06 13:19:17 -0600

    Add Text to Card component

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Card } from 'react-native-elements';
+import { Text } from 'react-native';
 
 class About extends Component {
   static navigationOptions = {
@@ -9,7 +10,17 @@ class About extends Component {
   render() {
     return (
       <ScrollView>
-        <Card title='Our Mission'></Card>
+        <Card title='Our Mission'>
+          <Text>
+            We present a curated database of the best campsites in the vast
+            woods and backcountry of the World Wide Web Wilderness. We increase
+            access to adventure for the public while promoting safe and
+            respectful use of resources. The expert wilderness trekkers on our
+            staff personally verify each campsite to make sure that they are up
+            to our standards. We also present a platform for campers to share
+            reviews on campsites they have visited with each other.
+          </Text>
+        </Card>
         <Card title='Community Partners'></Card>
       </ScrollView>
     );
